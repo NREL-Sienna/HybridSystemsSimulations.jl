58fb2cbe97c93e6f94d2550602d7f1687c479246
rodrigomha <rodrigomha@gmail.com>
2023-02-22 17:44:20 -0800

    add test

--- src/HybridSystemsSimulations.jl
+++ src/HybridSystemsSimulations.jl
@@ -2,7 +2,9 @@ module HybridSystemsSimulations
 
 using DocStringExtensions
 import PowerSimulations
+import MathOptInterface
 const PSI = PowerSimulations
+const MOI = MathOptInterface
 
 @template (FUNCTIONS, METHODS) = """
                                  $(TYPEDSIGNATURES)

8a39d0e2311a01f62114ea0779bed12cfdfa9d9c
rodrigomha <rodrigomha@gmail.com>
2023-02-22 17:25:34 -0800

    add additional dependencies

--- src/HybridSystemsSimulations.jl
+++ src/HybridSystemsSimulations.jl
@@ -1,6 +1,8 @@
 module HybridSystemsSimulations
 
 using DocStringExtensions
+import PowerSimulations
+const PSI = PowerSimulations
 
 @template (FUNCTIONS, METHODS) = """
                                  $(TYPEDSIGNATURES)
--- src/formulations.jl
+++ src/formulations.jl
@@ -3,3 +3,4 @@ abstract type AbstractHybridFormulation <: AbstractDeviceFormulation end
 abstract type AbstractStandardHybridFormulation <: AbstractHybridFormulation end
 struct BasicHybridDispatch <: AbstractHybridFormulation end
 struct StandardHybridDispatch <: AbstractStandardHybridFormulation end
+struct HybridOptimizer <: PSI.DecisionModel end

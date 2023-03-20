376d2aeaed89d958518fcfe87e51660a33c51f76
rodrigomha <rodrigomha@gmail.com>
2023-03-19 20:41:20 -0600

    add hybrid to a system

--- scripts/modify_systems.jl
+++ scripts/modify_systems.jl
@@ -41,3 +41,40 @@ function add_battery_to_bus!(sys::System, bus_name::String)
     add_component!(sys, bat)
     return
 end
+
+function add_hybrid_to_chuhsi_bus!(sys::System)
+    bus = get_component(Bus, sys, "Chuhsi")
+    bat = _build_battery(bus, 4.0, 2.0, 0.93, 0.93)
+    # Wind is taken from Bus 317: Chuhsi
+    # Thermal and Load is taken from adjacent bus 318: Clark
+    ren_name = "317_WIND_1"
+    thermal_name = "318_CC_1"
+    load_name = "Clark"
+    renewable = get_component(StaticInjection, sys, ren_name)
+    thermal = get_component(StaticInjection, sys, thermal_name)
+    load = get_component(PowerLoad, sys, load_name)
+    # Create the Hybrid
+    hybrid_name = string(bus.number) * "_Hybrid"
+    hybrid = PSY.HybridSystem(
+        name=hybrid_name,
+        available=true,
+        status=true,
+        bus=bus,
+        active_power=1.0,
+        reactive_power=0.0,
+        base_power=100.0,
+        operation_cost=TwoPartCost(nothing),
+        thermal_unit=thermal, #new_th,
+        electric_load=load, #new_load,
+        storage=bat,
+        renewable_unit=renewable, #new_ren,
+        interconnection_impedance=0.0 + 0.0im,
+        interconnection_rating=nothing,
+        input_active_power_limits=(min=0.0, max=10.0),
+        output_active_power_limits=(min=0.0, max=10.0),
+        reactive_power_limits=nothing,
+    )
+    # Add Hybrid
+    add_component!(sys, hybrid)
+    return
+end

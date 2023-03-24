c9dafcc92c2b15634aaed157f28e3e5e90c38e70
rodrigomha <rodrigomha@gmail.com>
2023-03-24 16:02:11 -0600

    remove extra formulation

--- src/formulations.jl
+++ src/formulations.jl
@@ -1,4 +1,3 @@
 ########################### Hybrid Generation Formulations ################################
 abstract type AbstractHybridFormulation <: PSI.AbstractDeviceFormulation end
 struct HybridEnergyOnlyDispatch <: AbstractHybridFormulation end
-#struct HybridEnergyOnlyDispatch <: AbstractHybridFormulation end

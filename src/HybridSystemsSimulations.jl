b9a53b5ad04f670117a4588a5c64b68ab0429e00
rodrigomha <rodrigomha@gmail.com>
2023-03-27 11:12:05 -0700

    update names

--- src/HybridSystemsSimulations.jl
+++ src/HybridSystemsSimulations.jl
@@ -36,7 +36,7 @@ include("parameters_definitions.jl")
 include("variables_definitions.jl")
 include("constraints_definitions.jl")
 include("hybrid_decision_models.jl")
-include("hybridsystems_device_models.jl")
-include("hybridgeneration_constructor.jl")
+include("hybrid_device_models.jl")
+include("hybrid_constructor.jl")
 
 end

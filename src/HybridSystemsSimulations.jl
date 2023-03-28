aaac8af83c1de1d7c5fc12f24acf1cf7c82850c6
rodrigomha <rodrigomha@gmail.com>
2023-03-28 11:19:47 -0700

    update names

--- src/HybridSystemsSimulations.jl
+++ src/HybridSystemsSimulations.jl
@@ -22,6 +22,7 @@ const PSI = PowerSimulations
 const PSY = PowerSystems
 const PM = PSI.PM
 const IS = PSI.IS
+const PNM = PSI.PNM
 
 using DocStringExtensions
 @template (FUNCTIONS, METHODS) = """
@@ -34,8 +35,8 @@ include("formulations.jl")
 include("parameters_definitions.jl")
 include("variables_definitions.jl")
 include("constraints_definitions.jl")
-include("hybrid_decision_models.jl")
-include("hybrid_device_models.jl")
-include("hybrid_constructor.jl")
+include("hybrid_system_decision_models.jl")
+include("hybrid_system_device_models.jl")
+include("hybrid_system_constructor.jl")
 
 end

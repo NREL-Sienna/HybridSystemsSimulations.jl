ebeb4cf2970b99e6f227f2b7a4a86d0bcb2daf24
Jose Daniel Lara <jdlara@berkeley.edu>
2023-07-21 10:50:25 -0600

    add nee decision model

--- src/core/decision_models.jl
+++ src/core/decision_models.jl
@@ -3,3 +3,4 @@ abstract type HybridDecisionProblem <: PSI.DecisionProblem end
 struct MerchantHybridEnergyCase <: HybridDecisionProblem end
 struct MerchantHybridEnergyFixedDA <: HybridDecisionProblem end
 struct MerchantHybridCooptimizerCase <: HybridDecisionProblem end
+struct MerchantHybridBilevelCase <: HybridDecisionProblem end

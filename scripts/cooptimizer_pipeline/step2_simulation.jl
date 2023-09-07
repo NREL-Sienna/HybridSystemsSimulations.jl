821cdb9a538514e653cf0fecc4ee9aba9002dc06
rodrigomha <rodrigomha@gmail.com>
2023-09-07 16:08:21 -0700

    add cooptimizer script

--- scripts/cooptimizer_pipeline/step2_simulation.jl
+++ scripts/cooptimizer_pipeline/step2_simulation.jl
@@ -22,9 +22,43 @@ transform_single_time_series!(sys_rts_da, horizon_DA, interval_DA)
 transform_single_time_series!(sys_rts_merchant_da, horizon_DA * 12, interval_DA)
 transform_single_time_series!(sys_rts_merchant_rt, horizon_RT, interval_RT)
 
-dic = PSY.get_ext(sys_rts_merchant_da)
 
-# Add prices to ext. Only three days.
+mipgap = 1e-2
+#########################################
+######## Add Services to Hybrid #########
+#########################################
+
+served_fraction_map = Dict(
+    "Spin_Up_R2" => 0.00,
+    "Spin_Up_R3" => 0.00,
+    "Reg_Up" => 0.3,
+    "Spin_Up_R1" => 0.00,
+    "Flex_Up" => 0.1,
+    "Reg_Down" => 0.3,
+    "Flex_Down" => 0.1,
+)
+
+for sys in [sys_rts_da, sys_rts_merchant_da, sys_rts_merchant_rt]
+    services = get_components(VariableReserve, sys)
+    hy_sys = first(get_components(HybridSystem, sys))
+    for service in services
+        serv_name = get_name(service)
+        serv_ext = get_ext(service)
+        serv_ext["served_fraction"] = served_fraction_map[serv_name]
+        if  contains(serv_name, "Spin_Up_R1") |
+            contains(serv_name, "Spin_Up_R2") |
+            contains(serv_name, "Flex")
+                continue
+        else
+            add_service!(hy_sys, service, sys)
+        end
+    end
+end
+
+#########################################
+########## Add Prices to EXT ############
+#########################################
+dic = PSY.get_ext(sys_rts_merchant_da)
 bus_name = "chuhsi"
 dic["λ_da_df"] =
     CSV.read("scripts/cooptimizer_pipeline/inputs/$(bus_name)_DA_prices.csv", DataFrame)
@@ -60,13 +94,248 @@ decision_optimizer_DA = DecisionModel(
     MerchantHybridCooptimizerCase,
     template_uc_copperplate,
     sys_rts_merchant_da,
-    optimizer=Xpress.Optimizer,
+    optimizer=optimizer_with_attributes(Xpress.Optimizer, "MIPRELSTOP" => mipgap),
     calculate_conflict=true,
+    optimizer_solve_log_print=false,
     system_to_file=false,
     store_variable_names=true;
     name="MerchantHybridCooptimizer_DA",
 )
 
+#=
 build!(decision_optimizer_DA, output_dir = mktempdir())
-
 solve!(decision_optimizer_DA)
+
+res = ProblemResults(decision_optimizer_DA)
+totbid_dn = res.aux_variable_values[PSI.AuxVarKey{HSS.TotalBidReserve, VariableReserve{ReserveDown}}("Reg_Down")]
+regdn_out = res.variable_values[PSI.VariableKey{BidReserveVariableOut, VariableReserve{ReserveDown}}("Reg_Down")]
+regdn_in = res.variable_values[PSI.VariableKey{BidReserveVariableIn, VariableReserve{ReserveDown}}("Reg_Down")]
+=#
+
+# Set Hybrid in UC as FixedDA
+set_device_model!(
+    template_uc_copperplate,
+    DeviceModel(
+        PSY.HybridSystem,
+        HybridWithReservesFixedDA;
+        attributes=Dict{String, Any}("cycling" => false),
+    ),
+)
+
+set_service_model!(template_uc_copperplate, ServiceModel(VariableReserve{ReserveUp}, RangeReserve, use_slacks = false))
+    set_service_model!(
+        template_uc_copperplate,
+        ServiceModel(VariableReserve{ReserveDown}, RangeReserve, use_slacks = false),
+    )
+
+######################################################
+####### Systems for RT Bids and Realized ED ##########
+######################################################
+rt_template = ProblemTemplate(CopperPlatePowerModel)
+set_device_model!(
+    rt_template,
+    DeviceModel(
+        PSY.HybridSystem,
+        HybridEnergyOnlyFixedDA;
+        attributes=Dict{String, Any}("cycling" => false),
+    ),
+)
+
+decision_optimizer_RT = DecisionModel(
+    MerchantHybridCooptimizerCase,
+    rt_template,
+    sys_rts_merchant_rt,
+    system_to_file=false,
+    optimizer=optimizer_with_attributes(Xpress.Optimizer, "MIPRELSTOP" => mipgap),
+    calculate_conflict=true,
+    store_variable_names=true;
+    name="MerchantHybridCooptimizer_RT",
+)
+
+decision_optimizer_RT.ext = Dict{String, Any}("RT" => true)
+
+
+
+# Construct decision models for simulation
+models = SimulationModels(
+    decision_models=[
+        decision_optimizer_DA,
+        DecisionModel(
+            template_uc_copperplate,
+            sys_rts_da;
+            name="UC",
+            optimizer=optimizer_with_attributes(Xpress.Optimizer, "MIPRELSTOP" => mipgap),
+            system_to_file=false,
+            initialize_model=true,
+            optimizer_solve_log_print=false,
+            calculate_conflict=true,
+            direct_mode_optimizer=true,
+            rebuild_model=false,
+            store_variable_names=true,
+            #check_numerical_bounds=false,
+        ),
+        decision_optimizer_RT,
+    ]
+)
+
+sequence = SimulationSequence(
+    models=models,
+    feedforwards=Dict(
+        "UC" => [
+            FixValueFeedforward(
+                component_type=PSY.HybridSystem,
+                source=EnergyDABidOut,
+                affected_values=[ActivePowerOutVariable],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.HybridSystem,
+                source=EnergyDABidIn,
+                affected_values=[ActivePowerInVariable],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveUp},
+                source=TotalBidReserve,
+                affected_values=[ActivePowerReserveVariable],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveDown},
+                source=TotalBidReserve,
+                affected_values=[ActivePowerReserveVariable],
+            ),
+        ],
+        "MerchantHybridCooptimizer_RT" => [
+            FixValueFeedforward(
+                component_type=PSY.HybridSystem,
+                source=EnergyDABidOut,
+                affected_values=[EnergyDABidOut],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.HybridSystem,
+                source=EnergyDABidIn,
+                affected_values=[EnergyDABidIn],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveUp},
+                source=BidReserveVariableOut,
+                affected_values=[BidReserveVariableOut],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveDown},
+                source=BidReserveVariableOut,
+                affected_values=[BidReserveVariableOut],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveUp},
+                source=BidReserveVariableIn,
+                affected_values=[BidReserveVariableIn],
+            ),
+            FixValueFeedforward(
+                component_type=PSY.VariableReserve{ReserveDown},
+                source=BidReserveVariableIn,
+                affected_values=[BidReserveVariableIn],
+            ),
+        ]
+    ),
+    ini_cond_chronology=InterProblemChronology(),
+)
+
+sim_steps = 3 # num_steps - 2
+sim = Simulation(
+    name="compact_sim",
+    steps=sim_steps,
+    models=models,
+    sequence=sequence,
+    initial_time=starttime,
+    simulation_folder=mktempdir(cleanup=true),
+)
+
+build!(sim)
+
+execute!(sim; enable_progress_bar=true)
+
+#=
+ucmod = sim.models.decision_models[2]
+vars = ucmod.internal.container.variables
+cons = ucmod.internal.container.constraints
+regdn_var = vars[PSI.VariableKey{ActivePowerReserveVariable, VariableReserve{ReserveDown}}("Reg_Down")]
+regdn_con = cons[PSI.ConstraintKey{RequirementConstraint, VariableReserve{ReserveDown}}("Reg_Down")]
+=#
+### Process Results
+
+results = SimulationResults(sim)
+result_merch_DA = get_decision_problem_results(results, "MerchantHybridCooptimizer_DA")
+result_merch_RT = get_decision_problem_results(results, "MerchantHybridCooptimizer_RT")
+results_uc = get_decision_problem_results(results, "UC")
+
+## Prices Comparison
+prices_uc_centralized = prices_uc_dcp
+prices_ed_centralized = prices_ed_dcp
+prices_uc_upd =
+    read_realized_dual(results_uc, "CopperPlateBalanceConstraint__System")[!, 2] ./ 100.0
+
+uc_out = read_realized_variable(results_uc, "ActivePowerOutVariable__HybridSystem")
+uc_in = read_realized_variable(results_uc, "ActivePowerOutVariable__HybridSystem")
+
+plot(
+    [
+        scatter(
+            x=dates_uc[1:(24 * sim_steps)],
+            y=prices_uc_centralized[1:(24 * sim_steps)],
+            name="Centralized DA Price",
+            line_shape="hv",
+        ),
+        scatter(x=dates_uc, y=prices_uc_upd, name="Simulation DA Price", line_shape="hv"),
+    ],
+    Layout(title="Price Differences", yaxis_title="\$/MWh"),
+)
+
+### Bid Comparison
+
+da_out = read_variable(result_merch_DA, "EnergyDABidOut__HybridSystem")
+da_out_realized = vcat([values(vdf)[!, 2][1:24] for vdf in values(da_out)]...)
+da_in = read_variable(result_merch_DA, "EnergyDABidIn__HybridSystem")
+da_in_realized = vcat([values(vdf)[!, 2][1:24] for vdf in values(da_in)]...)
+
+uc_p_out = read_realized_variable(results_uc, "ActivePowerOutVariable__HybridSystem")
+uc_p_in = read_realized_variable(results_uc, "ActivePowerInVariable__HybridSystem")
+
+da_out_rt = read_variable(result_merch_RT, "EnergyDABidOut__HybridSystem")
+da_out_rt_realized = vcat([values(vdf)[!, 2][1] for vdf in values(da_out_rt)]...)
+da_in_rt = read_variable(result_merch_RT, "EnergyDABidIn__HybridSystem")
+da_in_rt_realized = vcat([values(vdf)[!, 2][1] for vdf in values(da_in_rt)]...)
+
+p1 = plot([
+    scatter(x=dates_uc, y=da_out_realized, name="DA Bid Out", line_shape="hv"),
+    scatter(x=dates_uc, y=da_in_realized, name="DA Bid In", line_shape="hv"),
+    scatter(x=uc_p_out[!, 1], y=uc_p_out[!, 2] / 100.0, name="UC P Out", line_shape="hv"),
+    scatter(x=uc_p_in[!, 1], y=uc_p_in[!, 2] / 100.0, name="UC P In", line_shape="hv"),
+    scatter(
+        x=dates_uc,
+        y=da_out_rt_realized,
+        name="DA Bid Out RT",
+        line_shape="hv",
+    ),
+    scatter(
+        x=dates_uc,
+        y=da_in_rt_realized,
+        name="DA Bid In RT",
+        line_shape="hv",
+    ),
+])
+
+
+regup_da_out = read_variable(result_merch_DA, "BidReserveVariableOut__VariableReserve__ReserveUp__Reg_Up")
+regup_da_out_realized = vcat([values(vdf)[!, 2][1:24] for vdf in values(regup_da_out)]...)
+
+regup_da_in = read_variable(result_merch_DA, "BidReserveVariableIn__VariableReserve__ReserveUp__Reg_Up")
+regup_da_in_realized = vcat([values(vdf)[!, 2][1:24] for vdf in values(regup_da_in)]...)
+
+regup_uc_out = read_realized_variable(results_uc, "ReserveVariableOut__VariableReserve__ReserveUp__Reg_Up")
+regup_uc_in = read_realized_variable(results_uc, "ReserveVariableIn__VariableReserve__ReserveUp__Reg_Up")
+
+p2 = plot([
+    scatter(x=dates_uc, y=regup_da_out_realized, name="RegUp DA Bid Out", line_shape="hv"),
+    scatter(x=dates_uc, y=regup_da_in_realized, name="RegUp DA Bid In", line_shape="hv"),
+    scatter(x=regup_uc_out[!, 1], y=regup_uc_out[!, 2], name="UC RegUp Out", line_shape="hv"),
+    scatter(x=regup_uc_in[!, 1], y=regup_uc_in[!, 2], name="UC RegUp In", line_shape="hv"),
+])

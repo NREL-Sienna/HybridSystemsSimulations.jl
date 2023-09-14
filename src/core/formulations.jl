4a97e64849d041935b530ccfaddfafc218e664bd
Jose Daniel Lara <jdlara@berkeley.edu>
2023-09-14 13:48:43 -0600

    use a single FixDA formulation

--- src/core/formulations.jl
+++ src/core/formulations.jl
@@ -3,8 +3,7 @@ abstract type AbstractHybridFormulation <: PSI.AbstractDeviceFormulation end
 abstract type AbstractHybridFormulationWithReserves <: AbstractHybridFormulation end
 struct HybridDispatchWithReserves <: AbstractHybridFormulationWithReserves end
 struct HybridEnergyOnlyDispatch <: AbstractHybridFormulation end
-struct HybridEnergyOnlyFixedDA <: AbstractHybridFormulation end
-struct HybridWithReservesFixedDA <: AbstractHybridFormulation end
+struct HybridFixedDA <: AbstractHybridFormulation end
 
 struct MerchantModelEnergyOnly <: AbstractHybridFormulation end
 struct MerchantModelWithReserves <: AbstractHybridFormulationWithReserves end

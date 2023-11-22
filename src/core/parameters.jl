d36469b8ac7a347792d69f97fd78f3f7e0749981
rodrigomha <rodrigomha@gmail.com>
2023-11-21 17:04:18 -0800

    add reserve slack cost for hybrids

--- src/core/parameters.jl
+++ src/core/parameters.jl
@@ -1,5 +1,6 @@
 const CYCLES_PER_DAY = 1.37
 const HOURS_IN_DAY = 24
+const RESERVE_SLACK_COST = 3000.0
 
 struct RenewablePowerTimeSeries <: PSI.TimeSeriesParameter end
 struct ElectricLoadTimeSeries <: PSI.TimeSeriesParameter end

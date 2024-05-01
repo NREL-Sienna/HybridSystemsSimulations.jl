f9b4adeca6385577d15afe1b4628606228be73f7
rodrigomha <rodrigomha@gmail.com>
2024-05-01 12:57:26 -0700

    update ff

--- src/feedforwards.jl
+++ src/feedforwards.jl
@@ -3,7 +3,7 @@ struct CyclingChargeLimitFeedforward <: PSI.AbstractAffectFeedforward
     affected_values::Vector{<:PSI.OptimizationContainerKey}
     target_period::Int
     penalty_cost::Float64
-    function CyclingLimitFeedforward(;
+    function CyclingChargeLimitFeedforward(;
         component_type::Type{<:PSY.Component},
         source::Type{T},
         affected_values::Vector{DataType},
@@ -18,7 +18,7 @@ struct CyclingChargeLimitFeedforward <: PSI.AbstractAffectFeedforward
                     PSI.get_optimization_container_key(v(), component_type, meta)
             else
                 error(
-                    "ReservoirTargetFeedforward is only compatible with VariableType or ParamterType affected values",
+                    "CyclingChargeLimitFeedforward is only compatible with VariableType or ParamterType affected values",
                 )
             end
         end
@@ -33,6 +33,43 @@ end
 PSI.get_default_parameter_type(::CyclingChargeLimitFeedforward, _) =
     CyclingChargeLimitParameter
 PSI.get_optimization_container_key(ff::CyclingChargeLimitFeedforward) =
+    ff.optimization_container_key
+
+struct CyclingDischargeLimitFeedforward <: PSI.AbstractAffectFeedforward
+    optimization_container_key::PSI.OptimizationContainerKey
+    affected_values::Vector{<:PSI.OptimizationContainerKey}
+    target_period::Int
+    penalty_cost::Float64
+    function CyclingDischargeLimitFeedforward(;
+        component_type::Type{<:PSY.Component},
+        source::Type{T},
+        affected_values::Vector{DataType},
+        target_period::Int,
+        penalty_cost::Float64,
+        meta=PSI.CONTAINER_KEY_EMPTY_META,
+    ) where {T}
+        values_vector = Vector{PSI.VariableKey}(undef, length(affected_values))
+        for (ix, v) in enumerate(affected_values)
+            if v <: PSI.VariableType
+                values_vector[ix] =
+                    PSI.get_optimization_container_key(v(), component_type, meta)
+            else
+                error(
+                    "CyclingDischargeLimitFeedforward is only compatible with VariableType or ParamterType affected values",
+                )
+            end
+        end
+        new(
+            PSI.get_optimization_container_key(T(), component_type, meta),
+            values_vector,
+            penalty_cost,
+        )
+    end
+end
+
+PSI.get_default_parameter_type(::CyclingDischargeLimitFeedforward, _) =
+    CyclingDischargeLimitParameter
+PSI.get_optimization_container_key(ff::CyclingDischargeLimitFeedforward) =
     ff.optimization_container_key
 
 function PSI.update_parameter_values!(
